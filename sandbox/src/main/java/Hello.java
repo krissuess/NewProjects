480fb283d21cd4d976bcfcf0b49a45bd6b90075d
krissuess <justkristy@yandex.ru>
2020-11-21 18:49:40 +0300

    Points distance

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,5 +1,24 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Point p1 = new Point();
+        Point p2 = new Point();
+        p1.x=12;
+        p1.y=23;
+        p2.x=11;
+        p2.y=56;
+        double d=Hello.distance(p1,p2);
+        System.out.println(d);
+
+        double e = p1.distance(p2);
+        System.out.println(e);
     }
+
+    public static double distance(Point p1, Point p2) {
+        double ac=p2.y-p1.y;
+        double bc=p2.x-p1.x;
+        double ab=Math.sqrt(ac*ac+bc*bc);
+        return ab;
+    }
+
 }
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,11 @@
+public class Point {
+    public double x;
+    public double y;
+
+    public  double distance(Point p2) {
+        double ac=p2.y-this.y;
+        double bc=p2.x-this.x;
+        double ab=Math.sqrt(ac*ac+bc*bc);
+        return ab;
+    }
+}
